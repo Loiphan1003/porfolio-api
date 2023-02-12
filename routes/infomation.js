cc0b6f7ed3ef5a73e6e2d816be562e155926aef0
Loiphan1003 <phanvuloi001@gmail.com>
2023-02-12 15:16:13 +0700

    change api get info

--- routes/infomation.js
+++ routes/infomation.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { collection, getDocs, addDoc, doc, deleteDoc, setDoc } = require("firebase/firestore"); 
-const {db } = require('../config/firebase/firebaseConfig');
+const {db, storage } = require('../config/firebase/firebaseConfig');
+const { ref, uploadBytes } = require('firebase/storage');
+
 
 router.get('/', async (req, res) => {
 
     try {
 
-        let result = [];
+        let result = {};
 
         const querySnapshot = await getDocs(collection(db, 'info'));
         querySnapshot.forEach((doc) => {
-            result.push({...doc.data(), id: doc.id})
+            result = {...doc.data(), id: doc.id}
         })
 
         return res.json(result);
@@ -26,8 +28,18 @@ router.post('/update', async(req, res) => {
     try {
         let {name, email ,image} = req.body;
 
-        await setDoc(doc(db,'info', req.body.id), {name, email,image})
+        // await setDoc(doc(db,'info', req.body.id), {name, email,image})
+        
+
+        console.log(req.body);
+
+        // const storageRef = ref(storage, '/images');
+        
+        // uploadBytes(storageRef, image).then((snapshot) =>{
+        //     console.log('upload done');
+        // })
         res.status(200).json({ message: 'Chỉnh sửa thành công' })
+
     } catch (error) {
         console.log(error)
         return res.status(404).json({ message: 'Error' });
